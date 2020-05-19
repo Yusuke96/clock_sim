e7147d6a092ab67defe54c3228d3652724bf5f63
Yusuke96 <yuddrrlocm261@gmail.com>
2020-05-19 17:53:06 +0900

    push test 2

--- event_manager.cpp
+++ event_manager.cpp
@@ -6,5 +6,12 @@ EM::EM(){
 EM::~EM(){
 }
 
-double EM::Access(Packet p){}
+double EM::Register(double time,double range){        
+    pair<double,double> e;
+    e.first = time;//開始時刻
+    e.second = e.first + range;//終了時刻
+    events.push_back(e);
+}
 
+void EM::Show_events(){
+}
