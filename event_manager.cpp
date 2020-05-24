724cf97fa2e32d893ea200d4181564f190221b4a
Yusuke96 <yuddrrlocm261@gmail.com>
2020-05-24 19:53:55 +0900

    make tmp_global.cpp/tmp_global.hpp

--- event_manager.cpp
+++ event_manager.cpp
@@ -7,13 +7,22 @@ double EM::Register(double time,double range){
   pair<double,double> e; //イベント
   e.first = time;             //開始時刻
   e.second = e.first + range; //終了時刻
-  events.push_back(e);
+
+  //イベント挿入
+  if(events.back().second <= e.first){
+    events.push_back(e);
+  }else{//エラー
+    cout << "Error: Incorrect event insert at " << e.first << "." << endl;
+    exit(-1);
+  }
+  
 }
 
 void EM::Show_events(){
-  auto it = events.begin();
+  auto it = events.begin(); //イテレータ
   long i=0;
   while(it != events.end()){
     cout << "Event " << i << "\t" << *it.first << " ~ " << *it.second << endl; 
   }
+  
 }
